df7902dfdd6f3dd5e1540446a2184de9bb54e1fc
Ivan Krause <ivan.krause@gmail.com>
2023-10-13 17:36:30 +0100

    update settings.py for heroku url

--- fhadmin/settings.py
+++ fhadmin/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fhadmin.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['fhadmin-bac701e6375a.herokuapp.com', 'localhost']
 
 
 # Application definition
